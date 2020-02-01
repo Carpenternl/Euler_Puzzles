799b33449de69d182c02e089da559ec27dec7bf0
H Timmerman <hugo.timmerman@outlook.com>
2020-02-01 12:04:48 +0100

    created workspace

---
+++ EulerCode/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EulerCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int res = Digit_cancelling_fractions.calculate();
+        }
+    }
+}
