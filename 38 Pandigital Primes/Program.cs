631c470a0f63ccdecedf2921126c4da5cafc7783
Hugo <Hugo.t.95@hotmail.com>
2025-11-02 14:35:58 +0100

    Completed 38

--- 38 Pandigital Primes/Program.cs
+++ 38 Pandigital Primes/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
 Console.WriteLine("Hello, World!");
+
+
+List<int> pans = [];
+for (int i = 1; i < 10000; i++)
+{
+    StringBuilder concatDigits = new();
+    var multiplier = 1;
+    while (concatDigits.Length < 9)
+    {
+        concatDigits.Append(i * multiplier);
+        multiplier++;
+    }
+    if (concatDigits.Length == 9)
+    {
+        var isPandigital = true;
+        var digits = concatDigits.ToString();
+        for (char digit = '1'; digit < '9'; digit++)
+        {
+            if (!digits.Contains(digit))
+            {
+                isPandigital = false;
+                break;
+            }
+        }
+        if (isPandigital==true)
+            pans.Add(int.Parse(digits));
+    }
+}
+var outValue = pans.Aggregate(0, (acc, next) => next > acc ? next : acc);
+Console.WriteLine(outValue);
+
